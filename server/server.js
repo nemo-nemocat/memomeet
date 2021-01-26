aa72fd2fececb17c79a441b9011a8793a621cd30
dahaelee <dah3132@gmail.com>
2021-01-26 18:49:32 +0900

    Revert "testt"
    This reverts commit b20f0e0c6bdfd32a9dda963d860b5bdd43cc8fa3.

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "build")));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+}
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
