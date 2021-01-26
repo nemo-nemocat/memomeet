65b432c8c03651b10a91fc23a8fa7b7e704c60eb
dahaelee <dah3132@gmail.com>
2021-01-26 18:49:58 +0900

    Revert "Update mysql-db.js"
    This reverts commit 712f411eea30be84622a615549492407c8d06c92.

--- server/mysql-db.js
+++ server/mysql-db.js
@@ -1,18 +1,11 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     post: 3000,
-//     user: 'root',
-//     password: 'root',
-//     database: 'memomeet'
-// });
-
 const connection = mysql.createConnection({
-    host: 'us-cdbr-east-03.cleardb.com',
-    user: 'b5dfcc92d33e0e',
-    password: '0c8450fd',
-    database: 'heroku_9c78ff95d911e67'
+    host: 'localhost',
+    post: 3000,
+    user: 'root',
+    password: 'root',
+    database: 'memomeet'
 });
 
 module.exports = connection;
