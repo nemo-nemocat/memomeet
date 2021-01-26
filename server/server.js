0dc9ae2537447f2e295834d4f4fd30beb336a3d7
dahaelee <dah3132@gmail.com>
2021-01-26 18:49:45 +0900

    Revert "test"
    This reverts commit b61f9bf3ff14da7b102bc9d20619098cbb179b6c.

--- server/server.js
+++ server/server.js
@@ -14,12 +14,8 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
 /************************************ 화상채팅용 코드 시작 ************************************/
