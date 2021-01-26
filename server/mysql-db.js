712f411eea30be84622a615549492407c8d06c92
dahaelee <dah3132@gmail.com>
2021-01-26 18:04:14 +0900

    Update mysql-db.js

--- server/mysql-db.js
+++ server/mysql-db.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql");
 
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     post: 3000,
+//     user: 'root',
+//     password: 'root',
+//     database: 'memomeet'
+// });
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    post: 3000,
-    user: 'root',
-    password: 'root',
-    database: 'memomeet'
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'b5dfcc92d33e0e',
+    password: '0c8450fd',
+    database: 'heroku_9c78ff95d911e67'
 });
 
 module.exports = connection;
