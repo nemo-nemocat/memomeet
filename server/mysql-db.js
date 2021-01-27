895b6cdecff6256f017cc3a4a8917127336b7a0a
dahaelee <dah3132@gmail.com>
2021-01-27 14:06:32 +0900

    add .slugignore, DB connect

--- server/mysql-db.js
+++ server/mysql-db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    post: 3000,
-    user: 'root',
-    password: 'root',
-    database: 'memomeet'
+    host: 'us-cdbr-east-03.cleardb.com',
+    post: 3306,
+    user: 'b5dfcc92d33e0e',
+    password: '0c8450fd',
+    database: 'heroku_9c78ff95d911e67'
 });
 
 module.exports = connection;
