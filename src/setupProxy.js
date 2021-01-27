58fb5198c7a175831f346610019286802f329aaf
dahaelee <dah3132@gmail.com>
2021-01-27 12:35:05 +0900

    proxy err debug

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,10 @@ module.exports = function (app) {
     app.use(["/finishedmeet-info","/finishedmeet-addtag","/finishedmeet-deletetag","/meeting","/auth-login","/auth-logout","/auth-signup","/group-show", "/group-memberlist", "/group-search","/group-enter", "/group-out", "/group-create","/forwardmeet-create", "/forwardmeet-list", "/forwardmeet-delete",'/socket.io'],createProxyMiddleware({
         target: 'http://localhost:3002',
         changeOrigin: true,
+    }));
+    app.use(["/meeting",'/socket.io'],createProxyMiddleware({
+        target: 'http://localhost:3002',
+        changeOrigin: true,
         ws: true,
     }));
 };
