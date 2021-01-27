cfbb11437142d721af920ca52f53e09a765b5fb7
hyeppy <hyerin226cs@gmail.com>
2021-01-27 15:45:25 +0900

    modify script page layout

--- src/Components/summary.js
+++ src/Components/summary.js
@@ -1,61 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Paper, Typography, Chip, Button, Grid} from '@material-ui/core';
-import EventIcon from '@material-ui/icons/Event';
-import ScheduleIcon from '@material-ui/icons/Schedule';
-import ChipInput from 'material-ui-chip-input';
-import GetAppIcon from '@material-ui/icons/GetApp';
+import {Paper, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        width:"90%",
-        height:"20%",
-        margin:"1%"
+        minWidth:250,
+        width:"30%",
+        height:"90%",
+        marginTop:"1%"
     },
     paper: {
-        marginLeft:"5%",
+        marginLeft:"1%",
         width:"100%",
-        marginTop:"1%",
         height:"100%", 
         paddingTop:"1%",
-    },
-    Chip: {
-        backgroundColor: "#ffc31e",
-        marginLeft:"1%",
-        height:"10%",
-    },
-    TagChip: {
-        marginLeft:"1%",
-        marginTop:"0.5%",
+        marginTop:"1%",
     },
     data: {
         overflow:"auto",
-        height:"100%",
+        height:"97%",
         '&::-webkit-scrollbar' : {
             display : 'none'
-        }
+        },
+        paddingTop:"1%",
+        paddingLeft:"1%",
+        paddingRight:"1%"
     },
-    button: {
-        marginTop:"1%",
-    },
-    grid: {
-        textAlign:"left",
-        marginLeft:"4%"
-    }
 }));
-
-const chipRenderer = ({ chip, className, handleClick, handleDelete }, key) => (
-    <Chip
-      className={className}
-      key={key}
-      label={chip}
-      onClick={handleClick}
-      onDelete={handleDelete}
-      size="small"
-    />
-  );
-  
-  const defaultValue = ["주제 선정", "영어 동화", "화상 회의"];
 
 export default function Script(prop) {
     const classes = useStyles();
@@ -89,26 +60,16 @@ export default function Script(prop) {
 
     return (
         <div className={classes.root}>
-            <Grid>
-                <Typography variant="h6" align="left" style={{width:"100%"}}>
-                    <span style={{fontWeight:"bold", marginLeft:"5%"}}>졸업 프로젝트</span>
-                </Typography>
-            </Grid>
-            <Grid className={classes.grid}>
-                <Chip className={classes.Chip} id="meet_day" icon={<EventIcon/>} label="2021-01-21" />
-                <Chip className={classes.Chip} id="meet_time" icon={<ScheduleIcon/>} label="13:00"/>
-                <ChipInput className={classes.TagChip} chipRenderer={chipRenderer} defaultValue={defaultValue} />
-            </Grid>
             <Paper elevation={3} className={classes.paper}>
                 <Typography variant="h6" align="center">
                     <span style={{fontWeight: "bold", textDecoration:"underline overline", textDecorationColor:"#ffc31e"}}>Summary</span>   
                 </Typography>
-                <div style={{backgroundColor:"#eaeaea", width:"90%", height:"65%",borderRadius:10, margin:"auto"}}>
+                <div style={{backgroundColor:"#eaeaea", width:"90%", height:"90%",borderRadius:10, margin:"auto"}}>
                     <Typography className={classes.data}>
                     
                     </Typography>
                 </div>
-            </Paper> 
+            </Paper>
         </div>
     );
 }
