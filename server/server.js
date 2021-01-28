911cc669398e05bb4969ae68794bb3003ae126d4
dahaelee <dah3132@gmail.com>
2021-01-28 12:00:16 +0900

    test7

--- server/server.js
+++ server/server.js
@@ -17,9 +17,19 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, '../client/build'))); 
   
-  app.get('(?!^meeting$)', (req, res) => { // 일반 페이지는 react 빌드 파일로 라우트
+  app.get('/main', (req, res) => { // 일반 페이지는 react 빌드 파일로 라우트
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
+  app.get('/home', (req, res) => { // 일반 페이지는 react 빌드 파일로 라우트
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  app.get('/signup', (req, res) => { // 일반 페이지는 react 빌드 파일로 라우트
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  app.get('/script', (req, res) => { // 일반 페이지는 react 빌드 파일로 라우트
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
 }
 
 /************************************ 화상채팅용 코드 시작 ************************************/
