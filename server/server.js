39c075ee6f67b648eb2ac3ad2673bd8d1b4c1152
dahaelee <dah3132@gmail.com>
2021-02-08 19:40:32 +0900

    https redirecting complete

--- server/server.js
+++ server/server.js
@@ -14,13 +14,15 @@ app.use(bodyParser.json());
 
 /************************************************** SSL **************************************************/
 
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`)
-  } else {
-    next();
-  }
-});
+if (process.env.NODE_ENV == 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+      next();
+    }
+  });
+}
 
 /************************************************** DATABASE **************************************************/
 
