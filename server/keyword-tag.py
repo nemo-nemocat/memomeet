bfd32391c929c1c21ca6da30af1a4bd17cfc1169
dahaelee <dah3132@gmail.com>
2021-02-10 16:08:23 +0900

    eunjeon

--- server/keyword-tag.py
+++ server/keyword-tag.py
@@ -4,7 +4,7 @@ import base64
 
 from collections import Counter
 from nltk.corpus import stopwords
-from konlpy.tag import Mecab
+from eunjeon import Mecab
 
 def get_noun(news, stopwords):
 # def get_noun(news):
