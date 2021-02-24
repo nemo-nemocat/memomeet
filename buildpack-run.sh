5d306f4c2ad2c8f3ac6136a5944a020fe1e4c182
dahaelee <dah3132@gmail.com>
2021-02-24 19:32:43 +0900

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,1 +1,10 @@
-curl -s https://raw.githubusercontent.com/koshort/pyeunjeon/master/scripts/mecab.sh
+# install mecab-ko
+cd /tmp
+curl -LO https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz
+tar zxfv mecab-0.996-ko-0.9.2.tar.gz
+cd mecab-0.996-ko-0.9.2
+./configure
+make
+make check
+sudo make install
+sudo ldconfig  # In case of error: libmecab.so.2: cannot open shared object file: No such file or directory
