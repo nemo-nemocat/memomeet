a286e140f65283178ca47a59c137f920b810aa0a
dahaelee <dah3132@gmail.com>
2021-03-05 17:41:44 +0900

    Update app.py

--- server/app.py
+++ server/app.py
@@ -16,6 +16,7 @@ env = os.environ.get("PYTHON_ENV")
 if(env =="production"):
     import mecab
     mecab = mecab.MeCab()
+    print('배포버전배포버전배포버전')
 else :
     from eunjeon import Mecab
     mecab = Mecab()
