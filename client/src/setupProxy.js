02116eeb67739d4848081da649d0449384d3385b
dahaelee <dah3132@gmail.com>
2021-05-04 16:24:15 +0900

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
         changeOrigin: true,
     }));
     app.use(["/meeting",'/socket.io'],createProxyMiddleware({
-        target: 'https://memomeet.herokuapp.com',
+        target: 'http://localhost:3002',
         changeOrigin: true,
         ws: true,
     }));
